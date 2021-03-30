3a79395d574228c875cd5ec9c10db996f6e3c9c4
[khushbu2892] <khushbu.s@cearsinfotech.com>
2021-03-30 17:39:54 +0530

    Initial commit

--- Sources/swifiDependencyPackage/swifiDependencyPackage.swift
+++ Sources/swifiDependencyPackage/swifiDependencyPackage.swift
@@ -1,3 +1,16 @@
+import Foundation
+import UIKit
+
 struct swifiDependencyPackage {
     var text = "Hello, World!"
+}
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+            red:   .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue:  .random(in: 0...1),
+           alpha: 1.0
+        )
+    }
 }
