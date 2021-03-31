2333540945beeb3d5521969dd2ed46b07dbe94e7
[khushbu2892] <khushbu.s@cearsinfotech.com>
2021-03-31 09:57:07 +0530

    1.0.2tag add

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
 //        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.2.1"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .branch("master"))
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .exact("4.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
