4581d2c71b707bf31a895942f0d473cd8bbbe01d
[khushbu2892] <khushbu.s@cearsinfotech.com>
2021-03-31 09:38:11 +0530

    Update

--- Package.swift
+++ Package.swift
@@ -16,14 +16,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.2.1"),
+//        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.2.1"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .exact("4.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swifiDependencyPackage",
-            dependencies: []),
+            dependencies: ["Kingfisher"]),
         .testTarget(
             name: "swifiDependencyPackageTests",
             dependencies: ["swifiDependencyPackage"]),
